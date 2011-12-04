c77598301de17341ee943ebc8c57753553ee9bae
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-12-03 17:19:02 -0800

    WSDL <--> JSON and simple med query example

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ This is the smart_pf add-on.  It contains:
 * A program (lib/main.js).
 * A few tests.
 * Some meager documentation.
+
+> wget https://pfws.practicefusion.com/emr/2.0.45.1/Chart.asmx?WSDL
+> node parse_wsdl.js Chart.asmx\?WSDL  > Chart.js 
+
+Load test.html to print a list of medications.
--- parse_wsdl.js
+++ parse_wsdl.js
@@ -1,31 +1,31 @@
-
 var wsdl;
 var util = require('util');
 var fs = require('fs'),
     xml2js = require('xml2js');
 
-var parser = new xml2js.Parser();
+var parser = new xml2js.Parser({explicitArray: true});
 
 var wsdl = {};
 
-fs.readFile('Chart.asmx?WSDL', function(err, data) {
+var arguments = process.argv;
+var infile = arguments[2];
+var libname = infile.split('.')[0];
+
+fs.readFile(infile, function(err, data) {
     parser.parseString(data, function (err, r) {
 
 	wsdl.types = {};
 
 	var addTypes = function(k, t) {
 	    if (t === undefined) return;
-	    if (t.constructor !== Array)
-		t = [t];
 	    t.forEach(function(type){
 		var v = {};
-		v[k] = type;
+		v[k] = [type];
 		wsdl.types[type['@']['name']] = v;
 	    });	    
 	};
-
-	addTypes('s:element', r['wsdl:types']['s:schema']['s:element']);
-	addTypes('s:simpleType', r['wsdl:types']['s:schema']['s:simpleType']);
+	addTypes('s:element', r['wsdl:types'][0]['s:schema'][0]['s:element']);
+	addTypes('s:simpleType', r['wsdl:types'][0]['s:schema'][0]['s:simpleType']);
 	addTypes('s:simpleType', [
 	    {'@':{name: 's:string'}},
 	    {'@':{name: 's:dateTime'}},
@@ -33,7 +33,7 @@ fs.readFile('Chart.asmx?WSDL', function(err, data) {
 	    {'@':{name: 's:long'}},
 	    {'@':{name: 's:int'}}
 	]);
-	addTypes('s:complexType', r['wsdl:types']['s:schema']['s:complexType']);
+	addTypes('s:complexType', r['wsdl:types'][0]['s:schema'][0]['s:complexType']);
 
 	wsdl.operations = {};
 	var ports = r['wsdl:portType'];
@@ -47,31 +47,28 @@ fs.readFile('Chart.asmx?WSDL', function(err, data) {
 	});
 
 	wsdl.services = {};
-	var ports= r['wsdl:service']['wsdl:port'];
+	var ports= r['wsdl:service'][0]['wsdl:port'];
 	ports.forEach(function(port) {
 	    Object.keys(port).forEach(function(k) {
 		if (k === '@') return;
-		wsdl.services[port['@'].binding] = port[k]['@'].location;
+		wsdl.services[port['@'].binding] = port[k][0]['@'].location;
 	    });
 	});
 
 	wsdl.messages = {};
 	var messages= r['wsdl:message'];
 	messages.forEach(function(message) {
-	    wsdl.messages[message['@'].name] = message['wsdl:part']['@'].element;
+	    wsdl.messages[message['@'].name] = message['wsdl:part'][0]['@'].element;
 	});
 
-
-	wsdl.bindings = {};
 	var bindings = r['wsdl:binding'];
 	bindings.forEach(function(binding) {
 	    var operations = binding['wsdl:operation'];
 	    if (!operations) return;
-
 	    
 	    operations.forEach(function(operation) {
 		if ( operation['soap12:operation'] ) {
-		    wsdl.operations[operation['@'].name].soapAction = operation['soap12:operation']['@'].soapAction;
+		    wsdl.operations[operation['@'].name].soapAction = operation['soap12:operation'][0]['@'].soapAction;
 		    wsdl.operations[operation['@'].name].service = wsdl.services[binding['@'].type];
 		}
 	    });
@@ -79,13 +76,15 @@ fs.readFile('Chart.asmx?WSDL', function(err, data) {
 
 	Object.keys(wsdl.operations).forEach(function(k){
 	    var o = wsdl.operations[k];
-	    var inmsg = striptns(o['wsdl:input']['@'].message);
+	    var inmsg = striptns(o['wsdl:input'][0]['@'].message);
 	    o.intype = wsdl.messages[inmsg];
 
-	    var outmsg = striptns(o['wsdl:output']['@'].message);
+	    var outmsg = striptns(o['wsdl:output'][0]['@'].message);
 	    o.outtype = wsdl.messages[outmsg];
 	});
 
+
+/*
 	var b = new WB(wsdl, {
 	    i_lProviderID: 123,
 	    strToken: "ha",
@@ -98,139 +97,20 @@ fs.readFile('Chart.asmx?WSDL', function(err, data) {
 	    i_strSearchString: "S",
 	    i_eTemplateFieldToSearch: "O"
 	});
+*/
+	console.log("var PF = (PF || {});");
+	console.log("PF." + libname+" = (PF." + libname+" || {});");
+	console.log("PF." + libname+".wsdl = "+JSON.stringify(wsdl, null, 2));
+	console.log("WB.add_service(PF."+libname+");");
+
+
 
     });
 });
 
-var WB = function(wsdl, defaults) {
-    this.wsdl = wsdl;
-    this.defaults = defaults || {};
-};
 
 striptns = function(t) {
     var m = t.match(/^tns:(.*)/);
     if (m !== null) return m[1];
     return t;
 };
-
-WB.prototype.serialize_type  = function(type, json_values, path_here) {
-//    console.log(type);
-
-    if (type.constructor === String) {
-	type = striptns(type);
-	type = this.wsdl.types[type];
-//	console.log(type);
-    }
-
-    if ( type['s:element'] ) {
-	var e = type['s:element'];
-	var n = e['@'].name;
-	path_here = [n];
-
-	var v;
-
-	if ( e['@']['type'] ) {
-	    v = this.serialize_type(this.wsdl.types[e['@']['type']], json_values, path_here);
-	}
-	else if ( e['s:simpleType'] ) {
-	    v =  this.serialize_simple_type(e['s:simpleType'], json_values, path_here);
-	}
-	else if ( e['s:complexType'] ) {
-	    v =  this.serialize_complex_type(e['s:complexType'], json_values, path_here);
-	}
-
-	return this.xinset(n,v, path_here.length);
-
-    }
-
-    else if ( type['s:simpleType'] ) {
-	return this.serialize_simple_type(type['s:simpleType'], json_values, path_here);
-    }
-    else if ( type['s:complexType'] ) {
-	return this.serialize_complex_type(type['s:complexType'], json_values, path_here);
-    }
-
-};
-
-WB.prototype.serialize_simple_type = function(type, json_values, path_here) {
-    var v = json_values;
-//    console.log("simple for "+JSON.stringify(type) + ": "  + path_here);
-    if (v=== undefined || v.constructor !== String) {
-	var d = this.defaults[path_here[path_here.length - 1]];
-	if (d === undefined) {
-	    throw "Simple type value not found: " + path_here.join(" -> ");
-	}
-	else
-	    json_values = d;
-    }
-    
-    
-
-    // todo check enumeration restrictions here too
-    return json_values;
-};
-
-WB.prototype.serialize_complex_type = function(type, json_values, path_here) {
-    var ret = [];
-    var _wb = this;
-
-    type['s:sequence']['s:element'].forEach(function(e) {
-	var n = e['@'].name;
-	var t = e['@'].type;
-	
-	var use_vals = json_values ? json_values[n] : undefined;
-
-	try {
-	    path_here.push(n);
-	    var v = _wb.serialize_type(t, use_vals, path_here)
-	    ret.push(_wb.xinset(n,v, path_here.length));
-	} catch(err) {
-	    if (e['@'].minOccurs !== '0')
-		throw err
-	} finally {
-	    path_here.pop(n);
-	}
-
-    });
-
-    return ret.join('');
-};
-
-WB.prototype.xinset = function(n, v, d) {
-    v = v.toString();
-
-    var indent = '';
-    var offset  = (v.match(/\n/) ? '\n' : '') ;
-
-    for (var i = 0; i < d; i++)
-	indent += '  ';
-    return (indent + '<'+n+'>' +
-	    offset +
-	    v + 
-	    (offset? indent : '')+
-	    '</'+n+'>\n');
-};
-
-WB.prototype.make_call = function(call, json_values) {
-    var o = this.wsdl.operations[call];
-    var body = this.serialize_type(o.intype, json_values);
-
-    var env_head = '\n\
-<?xml version="1.0" encoding="utf-8"?>\n\
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\n\
-<soap12:Body xmlns="http://PracticeFusion.com/BusinessLogic/WebServices">\n';
-    var env_foot = '</soap12:Body>\n\
-</soap12:Envelope>\n';
-
-    var payload = env_head + body + env_foot;
-
-    var headers = [
-	'soapaction: '+o.soapAction,
-	'Content-Length: '+ payload.length
-    ];
-
-
-    console.log(o.service);
-    console.log(headers);
-    console.log(payload);
-}
